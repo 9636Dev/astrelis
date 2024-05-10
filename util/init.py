b0590077f897a4d4a1ddaf5ed80d6f6e9007c86d
9636Dev <9636dev@gmail.com>
2024-05-10 11:14:28 +0200

    [CHORE] Removed legacy buildscript

--- util/init.py
+++ util/init.py
@@ -7,4 +7,4 @@ PROJECT_DIR = CURRENT_DIR.parent
 run_dir = os.path.join(PROJECT_DIR, 'run')
 if not os.path.exists(run_dir):
     os.mkdir(run_dir)
-    os.mkdir(os.path.join(run_dir, 'run'))
+    os.mkdir(os.path.join(run_dir, 'lib'))
