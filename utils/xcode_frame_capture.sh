cdeb89361f6ffd579b6eb691d859ca3df66d4369
9636Dev <9636dev@gmail.com>
2024-07-17 21:01:09 +0800

    Added utility script for debugging on apple platforms

---
+++ utils/xcode_frame_capture.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OUTPUT_FILE=frame.gputrace
+
+# Param should be executable
+if [ -z "$1" ]; then
+    echo "Usage: $0 <executable>"
+    exit 1
+fi
+
+RET_VALUE=$(METAL_CAPTURE_ENABLED=1 MVK_CONFIG_AUTO_GPU_CAPTURE_SCOPE=1 MVK_CONFIG_AUTO_GPU_CAPTURE_OUTPUT_FILE=$OUTPUT_FILE $1)
+echo "Return value: $RET_VALUE"
+exit 0
